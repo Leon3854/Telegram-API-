f8abe96beee680e95c2a8c36b88b018118f46f00
Leoned <frad109@mail.ru>
2026-05-01 21:00:44 +0300

    The work with the producer-service add

---
+++ producer-service/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NOTIFY_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost:5672'],
+          queue: 'telegram_notifications',
+          queueOptions: {
+            durable: true, // Чтобы сообщения не пропадали при рестарте Rabbit
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
